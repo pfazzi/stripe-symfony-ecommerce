d8c2b404c7c4d58619db247072a80340f98c78c0
Patrick Luca Fazzi <patrick91@live.it>
2019-08-07 22:01:12 +0200

    Annotated routes enabled routes

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -4,21 +4,26 @@ declare(strict_types=1);
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CheckoutController
+class CheckoutController extends AbstractController
 {
     /**
      * @Route(path="/cart/step-one", methods={"GET"})
      *
      * @param Request $request
+     * @return Response
      */
-    public function stepOneAction(Request $request): void
+    public function stepOneAction(Request $request): Response
     {
         $intent = \Stripe\PaymentIntent::create([
             'amount' => 2000,
             'currency' => 'eur',
         ]);
+
+        return $this->render('checkout/step-one.html.twig');
     }
 }
